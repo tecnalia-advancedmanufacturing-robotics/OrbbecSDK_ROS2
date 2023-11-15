420df01f33b913d01ac0b376eeff615bea9b03ef
hzcyf <yufeng@orbbec.com>
2023-11-15 17:15:54 +0800

    Delete the obsolete parameters in the launch file

--- orbbec_camera/launch/gemini2.launch.py
+++ orbbec_camera/launch/gemini2.launch.py
@@ -11,16 +11,16 @@ def generate_launch_description():
     # Declare arguments
     args = [
         DeclareLaunchArgument('camera_name', default_value='camera'),
-        DeclareLaunchArgument('depth_registration', default_value='false'),
-        DeclareLaunchArgument('serial_number', default_value=''),
-        DeclareLaunchArgument('usb_port', default_value=''),
-        DeclareLaunchArgument('device_num', default_value='1'),
+        DeclareLaunchArgument('log_level', default_value='none'),
+        DeclareLaunchArgument('connection_delay', default_value='100'),
+
         DeclareLaunchArgument('vendor_id', default_value='0x2bc5'),
         DeclareLaunchArgument('product_id', default_value=''),
-        DeclareLaunchArgument('enable_point_cloud', default_value='true'),
-        DeclareLaunchArgument('enable_colored_point_cloud', default_value='true'),
-        DeclareLaunchArgument('point_cloud_qos', default_value='default'),
-        DeclareLaunchArgument('connection_delay', default_value='100'),
+        DeclareLaunchArgument('device_num', default_value='1'),
+        DeclareLaunchArgument('serial_number', default_value=''),
+        DeclareLaunchArgument('usb_port', default_value=''),
+
+
         DeclareLaunchArgument('color_width', default_value='640'),
         DeclareLaunchArgument('color_height', default_value='360'),
         DeclareLaunchArgument('color_fps', default_value='15'),
@@ -30,6 +30,7 @@ def generate_launch_description():
         DeclareLaunchArgument('color_qos', default_value='default'),
         DeclareLaunchArgument('color_camera_info_qos', default_value='default'),
         DeclareLaunchArgument('enable_color_auto_exposure', default_value='true'),
+
         DeclareLaunchArgument('depth_width', default_value='640'),
         DeclareLaunchArgument('depth_height', default_value='400'),
         DeclareLaunchArgument('depth_fps', default_value='15'),
@@ -38,6 +39,7 @@ def generate_launch_description():
         DeclareLaunchArgument('flip_depth', default_value='false'),
         DeclareLaunchArgument('depth_qos', default_value='default'),
         DeclareLaunchArgument('depth_camera_info_qos', default_value='default'),
+
         DeclareLaunchArgument('ir_width', default_value='640'),
         DeclareLaunchArgument('ir_height', default_value='400'),
         DeclareLaunchArgument('ir_fps', default_value='15'),
@@ -47,22 +49,24 @@ def generate_launch_description():
         DeclareLaunchArgument('ir_qos', default_value='default'),
         DeclareLaunchArgument('ir_camera_info_qos', default_value='default'),
         DeclareLaunchArgument('enable_ir_auto_exposure', default_value='true'),
+
+        DeclareLaunchArgument('depth_registration', default_value='false'),
+        DeclareLaunchArgument('enable_point_cloud', default_value='true'),
+        DeclareLaunchArgument('enable_colored_point_cloud', default_value='true'),
+        DeclareLaunchArgument('point_cloud_qos', default_value='default'),
+
         DeclareLaunchArgument('enable_accel', default_value='false'),
         DeclareLaunchArgument('accel_rate', default_value='100hz'),
         DeclareLaunchArgument('accel_range', default_value='4g'),
         DeclareLaunchArgument('enable_gyro', default_value='false'),
         DeclareLaunchArgument('gyro_rate', default_value='100hz'),
         DeclareLaunchArgument('gyro_range', default_value='1000dps'),
-        DeclareLaunchArgument('liner_accel_cov', default_value='0.01'),
-        DeclareLaunchArgument('angular_vel_cov', default_value='0.01'),
+
         DeclareLaunchArgument('publish_tf', default_value='true'),
         DeclareLaunchArgument('tf_publish_rate', default_value='10.0'),
-        DeclareLaunchArgument('ir_info_url', default_value=''),
-        DeclareLaunchArgument('color_info_url', default_value=''),
-        DeclareLaunchArgument('log_level', default_value='none'),
+
         DeclareLaunchArgument('enable_publish_extrinsic', default_value='false'),
-        DeclareLaunchArgument('enable_d2c_viewer', default_value='false'),
-        DeclareLaunchArgument('enable_soft_filter', default_value='true'),
+
         DeclareLaunchArgument('enable_ldp', default_value='true'),
         DeclareLaunchArgument('enable_soft_filter', default_value='true'),
         DeclareLaunchArgument('soft_filter_max_diff', default_value='-1'),
@@ -72,6 +76,7 @@ def generate_launch_description():
         # Unbinned Sparse Default
         # Binned Sparse Default
         DeclareLaunchArgument('depth_work_mode', default_value=''),
+
         DeclareLaunchArgument('sync_mode', default_value='free_run'),
         DeclareLaunchArgument('depth_delay_us', default_value='0'),
         DeclareLaunchArgument('color_delay_us', default_value='0'),
